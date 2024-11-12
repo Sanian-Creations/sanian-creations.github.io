79d86f86dcde32168f78c4224cb64566590fd2fa
Sanian <sanian.creations@gmail.com>
2024-11-13 00:34:10 +0100

    my-input now selects conten on focus

--- docs/js/my-input.js
+++ docs/js/my-input.js
@@ -1,20 +1,32 @@
-
 class MyInput extends HTMLElement {
-	constructor() {
-		super(); 
-	}
+	#clicked;
 	
-	connectedCallback() {
+	constructor() {
+		super();
+		
 		this.contentEditable = true;
-		this.addEventListener("keydown", e => {
-			// do not insert newline characters into this input
-			if (e.code === "Enter") {
-				e.preventDefault(); 
-			}
-		});
+
+		this.addEventListener("keydown", this.#keydown);
+		this.addEventListener("focus", this.#focus);
+		this.addEventListener("mousedown", this.#mousedown);
+		this.addEventListener("blur", this.#blur);
 	}
+
+	#keydown(e) {
+		if (e.code === "Enter") {
+			e.preventDefault(); 
+		}
+	}
+	#focus(e) {
+		// select content on focus, but not when clicked (then it places the cursor where you clicked)
+		if (this.#clicked) return;
+		window.getSelection().selectAllChildren(this);
+	}
+	#mousedown(e) { this.#clicked = true; }
+	#blur(e) { this.#clicked = false; }
 }
 
-
+// cached variable
+const range = document.createRange();
 
 customElements.define("my-input", MyInput);
